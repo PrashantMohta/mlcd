033414560fc0c614ba339c86e86e79fdeb2bb427
Prashant Mohta <prashant.mohta@testbook.com>
2020-06-09 02:00:36 +0530

    fix the example to allow moving the window

--- mlcd_example.py
+++ mlcd_example.py
@@ -1,7 +1,12 @@
-import mlcd     #import the module
+import mlcd,pygame     #import the module
 mlcd.init(16,3) # initialize a 16x3 display
 #draw the three lines passed as a list
-mlcd.draw(["Hello",         
-           "     world",
-           "Mock LCD !!!"])
+while True:
+    mlcd.draw(["Hello",         
+               "     world",
+               "Mock LCD !!!"])
+    for event in pygame.event.get():
+        pass
+
+    
 
