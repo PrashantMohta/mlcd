bbe2e44dc60a3771d14170cec23ef3488e15932d
Prashant Mohta <prashant.mohta@testbook.com>
2020-06-09 02:24:16 +0530

    Update mlcd_example.py with scale

--- mlcd_example.py
+++ mlcd_example.py
@@ -1,7 +1,6 @@
 import mlcd,pygame     #import the module
-mlcd.init(16,3) # initialize a 16x3 display
+mlcd.init(16,3,3) # initialize a 16x3 display scaled 3x  
 #draw the three lines passed as a list
-while True:
     mlcd.draw(["Hello",         
                "     world",
                "Mock LCD !!!"])
