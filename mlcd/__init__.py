bc89142526b9d3126ac5eff169767dd6861076e9
josh-justjosh <54246291+josh-justjosh@users.noreply.github.com>
2020-06-08 21:50:30 +0100

    Adds scale parameter (#2)
    Allows the user to make the board bigger

--- mlcd/__init__.py
+++ mlcd/__init__.py
@@ -1,22 +1,25 @@
 import pygame
-def init(chars,lines):
+def init(chars,lines,scalein):
     global screen
     global myfont
+    global scale
+    scale = scalein
     pygame.init()
-    size = [12*chars,20*lines]
+    size = [12*chars*scale,20*lines*scale]
     screen= pygame.display.set_mode(size)
     pygame.display.set_caption("Mock LCD")
-    myfont = pygame.font.SysFont("monospace", 20)
+    myfont = pygame.font.SysFont("monospace", 20*scale)
 
 
 def draw(args):
     i=0;
     global screen
     global myfont
+    global scale
     screen.fill((0,0,0))#erase screen contents
     while(i<len(args)):
-        line= myfont.render(args[i], 2, (255,255,0))
-        screen.blit(line, (0, 20*i))
+        line= myfont.render(args[i], 2*scale, (255,255,0))
+        screen.blit(line, (0, 20*i*scale))
         i+=1
     pygame.display.flip()
         
